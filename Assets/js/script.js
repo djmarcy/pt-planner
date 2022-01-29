aa48d5d1fada9ea93173e54b0a7f78af22c1efed
djmarcy <dj_marcy@hotmail.com>
2022-01-28 21:50:55 -0700

    css, pseudocoding, and adding time blocks

---
+++ Assets/js/script.js
@@ -0,0 +1,9 @@
+// Variables
+
+// MomentJS & setInterval to ensure accurate timer
+
+// Conditional formatting to make rows in planner glow red for times already past, grey for the current hour, and green for times later in the day
+
+// Local storage set/retrieval for events planned for the day
+
+// Event listeners ensuring that clicks are properly registered so events are saved to their correct location
