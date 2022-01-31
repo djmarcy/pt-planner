766f16e16e653a93f70a90c7936efeddcf36dcf8
djmarcy <dj_marcy@hotmail.com>
2022-01-30 18:09:28 -0700

    Moment functionality (timer in jumbotron and time-dependent background colors)

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,10 +1,13 @@
 // Variables
 let timeBlock = document.getElementById("currentDay");
 let time = null;
+let currentTime = moment().format("H")
+let hour = document.querySelectorAll("[data-time]")
 
 // MomentJS & setInterval to ensure accurate timer
 // Display current time on page load or function updateTime refresh
 timeBlock.textContent = time;
+console.log(moment().format("h"));
 
 // Grab time on page load, and ensure after every setInterval iteration the time is re-pulled from moment JS and properly updated
 function updateTime() {
@@ -17,12 +20,17 @@ updateTime();
 setInterval(updateTime, 1000);
 
 // Conditional formatting to make rows in planner glow red for times already past, grey for the current hour, and green for times later in the day
-if (currentTime > moment().format("h")) {
-  console.log("do something");
-} else if (currentTime < moment().format("h")) {
-  console.log("do something else");
-} else {
-  console.log("do something entirely different");
+for (i = 0; i < hour.length; i++) {
+
+  let scheduledTime = parseInt(hour[i].dataset.time)
+
+  if (currentTime > scheduledTime) {
+    hour[i].classList.add("past")
+  } else if (currentTime < scheduledTime) {
+    hour[i].classList.add("future")
+  } else {
+    hour[i].classList.add("present")
+  }
 }
 
 // Local storage set/retrieval for events planned for the day
