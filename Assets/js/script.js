fa968db99df1df578cfe32ae4ab2fcd97b26ba05
djmarcy <dj_marcy@hotmail.com>
2022-02-01 22:53:20 -0700

    final upload! whoo!

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,13 +1,16 @@
 // Variables
 let timeBlock = document.getElementById("currentDay");
 let time = null;
-let currentTime = moment().format("H")
-let hour = document.querySelectorAll("[data-time]")
+let currentTime = moment().format("H");
+let hour = document.querySelectorAll("[data-time]");
+let target = null;
+let newTarget = null;
+let newEvent = null;
+let clearBtn = document.getElementById("clear-button");
 
 // MomentJS & setInterval to ensure accurate timer
 // Display current time on page load or function updateTime refresh
 timeBlock.textContent = time;
-console.log(moment().format("h"));
 
 // Grab time on page load, and ensure after every setInterval iteration the time is re-pulled from moment JS and properly updated
 function updateTime() {
@@ -21,18 +24,48 @@ setInterval(updateTime, 1000);
 
 // Conditional formatting to make rows in planner glow red for times already past, grey for the current hour, and green for times later in the day
 for (i = 0; i < hour.length; i++) {
-
-  let scheduledTime = parseInt(hour[i].dataset.time)
+  let scheduledTime = parseInt(hour[i].dataset.time);
 
   if (currentTime > scheduledTime) {
-    hour[i].classList.add("past")
+    hour[i].classList.add("past");
   } else if (currentTime < scheduledTime) {
-    hour[i].classList.add("future")
+    hour[i].classList.add("future");
   } else {
-    hour[i].classList.add("present")
+    hour[i].classList.add("present");
   }
 }
 
-// Local storage set/retrieval for events planned for the day
+// Track which input is being saved, stops function from initiating if anything but the save button is pressed
+function clickListen(e) {
+  // Ignores anything without an ID, or anything with ID that includes the string "input", which is how I differentiated my save button IDs and input IDs
+  if (e.target.id == "" || e.target.id.includes("input")) {
+    return;
+  } else {
+    target = e.target.id;
+    inputValue = document.getElementById("input" + target).value;
+  }
 
+  storeInfo(target, inputValue);
+}
+
+function storeInfo(target, inputValue) {
+  localStorage.setItem(target, JSON.stringify(inputValue));
+}
+
+// Retrieve saved data
+function getInfo() {
+  //
+  for (i = 9; i < 18; i++) {
+    newEvent = localStorage.getItem(i);
+    document.getElementById("input" + i).value = JSON.parse(newEvent);
+  }
+}
+
+getInfo();
+
+function clearLocal() {
+  localStorage.clear;
+}
 // Event listeners ensuring that clicks are properly registered so events are saved to their correct location
+document.addEventListener("click", clickListen);
+clearBtn.addEventListener("click", clearLocal);
